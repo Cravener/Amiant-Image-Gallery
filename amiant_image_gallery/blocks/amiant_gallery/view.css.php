faad450f111e6e45b5a397e616dd1b5417f50494
JeffPaetkau <jeff.paetkau@abccomm.com>
2012-06-12 09:52:46 -0700

    Update master

--- amiant_image_gallery/blocks/amiant_gallery/view.css.php
+++ amiant_image_gallery/blocks/amiant_gallery/view.css.php
@@ -1,34 +1,34 @@
-<?php    defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
+<?php     defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
 
 <style type="text/css" media="all">
 
-.AmiantImageGalleryBlock<?php  echo $bID?>{
+.AmiantImageGalleryBlock<?php   echo $randID?>{
 	position: relative;
 	margin: 0 auto;
 }
 
-.AmiantImageGalleryBlock<?php  echo $bID?> .AmiantImageGalleryTitle {
+.AmiantImageGalleryBlock<?php   echo $randID?> .AmiantImageGalleryTitle {
 	font-weight: bold;
 	font-size: 18px;
 }
 
-.AmiantImageGallery<?php  echo $bID?>{
-	width: <?php  echo $width?>px;
-	height: <?php  echo $height+25;?>px;
+.AmiantImageGallery<?php   echo $randID?>{
+	width: <?php   echo $width?>px;
+	height: <?php   echo $height+25;?>px;
 	text-align: center;
 }
 
-.AmiantImageGalleryThumbnailContainerWrapper<?php  echo $bID?> {
-	width: <?php  echo $maxThumbnailWidth?>px;
-	height: <?php  echo $maxThumbnailHeight?>px;
+.AmiantImageGalleryThumbnailContainerWrapper<?php   echo $randID?> {
+	width: <?php   echo $maxThumbnailWidth?>px;
+	height: <?php   echo $maxThumbnailHeight?>px;
 	float: left;
 	margin: 10px;
 	position: relative;
 }
 
-.AmiantImageGalleryThumbnailContainer<?php  echo $bID?> {
+.AmiantImageGalleryThumbnailContainer<?php   echo $randID?> {
 	width: 100%;
-	height: <?php  echo $maxThumbnailHeight?>px;
+	height: <?php   echo $maxThumbnailHeight?>px;
 	background-color: #FFFFFF;
 	border: 1px solid #EEEEEE;
 	-moz-box-shadow: 2px 2px 5px #CCCCCC;
@@ -38,35 +38,36 @@
 	position: relative;
 }
 
-.AmiantImageGalleryThumbnailContainer<?php  echo $bID?>:hover {
+.AmiantImageGalleryThumbnailContainer<?php   echo $randID?>:hover {
 	background-color: #EEEEEE;
 	border: 1px solid #DDDDDD;
 }
 
-.AmiantImageGalleryThumbnailContainerLoading<?php  echo $bID?> {
-	background-image: url('<?php  echo $this->getBlockURL()."/images/ajax-loader.gif"; ?>');
+.AmiantImageGalleryThumbnailContainerLoading<?php   echo $randID?> {
+	background-image: url('<?php   echo $this->getBlockURL()."/images/ajax-loader.gif"; ?>');
 	background-position: center center;
 	background-repeat: no-repeat;
 }
 
-.AmiantImageGalleryThumbnailContainerError<?php  echo $bID?> {
-	background-image: url('<?php  echo $this->getBlockURL()."/images/error.png"; ?>');
+.AmiantImageGalleryThumbnailContainerError<?php   echo $randID?> {
+	background-image: url('<?php   echo $this->getBlockURL()."/images/error.png"; ?>');
 	background-position: center center;
 	background-repeat: no-repeat;
 }
 
-.AmiantImageGalleryThumbnail<?php  echo $bID?> {
+.AmiantImageGalleryThumbnail<?php   echo $randID?> {
 	margin: 0 auto;
 	position: relative;
 	top: 50%;
 }
 
-.AmiantImageGalleryThumbnailContainer<?php  echo $bID?> .AmiantImageGalleryThumbnailLink {
+.AmiantImageGalleryThumbnailContainer<?php   echo $randID?> .AmiantImageGalleryThumbnailLink {
 	border: none;
 	text-decoration: none;
 }
 
-.AmiantImageGalleryBlockControlBar<?php  echo $bID?> {
+.AmiantImageGalleryBlockControlBar<?php   echo $randID?> {
+	display: none;
 	margin: 5px 0px 5px 0px;
 	background: #F5F5F5;
 	border: 1px solid #EEEEEE;
@@ -101,40 +102,40 @@
 .AmiantImageGalleryBlockPopup td.top_left {
 	width: 15px;
 	height: 20px;
-	background: url('<?php  echo $this->getBlockURL()."/images/popup_bubble_top_left_corner_arrow.png"; ?>') bottom right no-repeat;
+	background: url('<?php   echo $this->getBlockURL()."/images/popup_bubble_top_left_corner_arrow.png"; ?>') bottom right no-repeat;
 }
 
 .AmiantImageGalleryBlockPopup td.top_left_small {
 	width: 50px;
 	height: 20px;
-	background: url('<?php  echo $this->getBlockURL()."/images/popup_bubble_top.png"; ?>') bottom right repeat-x;
+	background: url('<?php   echo $this->getBlockURL()."/images/popup_bubble_top.png"; ?>') bottom right repeat-x;
 }
 
 .AmiantImageGalleryBlockPopup td.top_right {
 	width: 17px;
 	height: 20px;
-	background: url('<?php  echo $this->getBlockURL()."/images/popup_bubble_top_right_corner.png"; ?>') bottom left no-repeat;
+	background: url('<?php   echo $this->getBlockURL()."/images/popup_bubble_top_right_corner.png"; ?>') bottom left no-repeat;
 }
 
 .AmiantImageGalleryBlockPopup td.top_center {
 	width: 26px;
 	height: 20px;
-	background: url('<?php  echo $this->getBlockURL()."/images/popup_bubble_top_arrow.png"; ?>') bottom left no-repeat;
+	background: url('<?php   echo $this->getBlockURL()."/images/popup_bubble_top_arrow.png"; ?>') bottom left no-repeat;
 }
 
 .AmiantImageGalleryBlockPopup td.top {
 	height: 20px;
-	background: url('<?php  echo $this->getBlockURL()."/images/popup_bubble_top.png"; ?>') bottom left repeat-x;
+	background: url('<?php   echo $this->getBlockURL()."/images/popup_bubble_top.png"; ?>') bottom left repeat-x;
 }
 
 .AmiantImageGalleryBlockPopup td.left {
 	width: 15px;
-	background: url('<?php  echo $this->getBlockURL()."/images/popup_bubble_left.png"; ?>') top right repeat-y;
+	background: url('<?php   echo $this->getBlockURL()."/images/popup_bubble_left.png"; ?>') top right repeat-y;
 }
 
 .AmiantImageGalleryBlockPopup td.right {
 	width: 17px;
-	background: url('<?php  echo $this->getBlockURL()."/images/popup_bubble_right.png"; ?>') repeat-y;
+	background: url('<?php   echo $this->getBlockURL()."/images/popup_bubble_right.png"; ?>') repeat-y;
 }
 
 .AmiantImageGalleryBlockPopup td.center {
@@ -149,37 +150,37 @@
 .AmiantImageGalleryBlockPopup td.bottom_left {
 	width: 15px;
 	height: 17px;
-	background: url('<?php  echo $this->getBlockURL()."/images/popup_bubble_bottom_left_corner.png"; ?>') top right no-repeat;
+	background: url('<?php   echo $this->getBlockURL()."/images/popup_bubble_bottom_left_corner.png"; ?>') top right no-repeat;
 }
 
 .AmiantImageGalleryBlockPopup td.bottom_right {
 	width: 17px;
 	height: 17px;
-	background: url('<?php  echo $this->getBlockURL()."/images/popup_bubble_bottom_right_corner.png"; ?>') no-repeat; 
+	background: url('<?php   echo $this->getBlockURL()."/images/popup_bubble_bottom_right_corner.png"; ?>') no-repeat; 
 }
 
 .AmiantImageGalleryBlockPopup td.bottom {
 	height: 17px;
-	background: url('<?php  echo $this->getBlockURL()."/images/popup_bubble_bottom.png"; ?>') repeat-x;
+	background: url('<?php   echo $this->getBlockURL()."/images/popup_bubble_bottom.png"; ?>') repeat-x;
 }
 
-#AmiantImageGallerySlidesWrapper<?php  echo $bID?> {
-	width: <?php  echo $width?>px;
-	height: <?php  echo $height?>px;
+#AmiantImageGallerySlidesWrapper<?php   echo $randID?> {
+	width: <?php   echo $width?>px;
+	height: <?php   echo $height?>px;
 	text-align: center;
 }
 
-.AmiantImageGallerySlidesWrapper<?php  echo $bID?> {
+.AmiantImageGallerySlidesWrapper<?php   echo $randID?> {
 	
 }
 
-.AmiantImageGallerySlide<?php  echo $bID?> {
-	width: <?php  echo $width?>px;
-	height: <?php  echo $height?>px;
+.AmiantImageGallerySlide<?php   echo $randID?> {
+	width: <?php   echo $width?>px;
+	height: <?php   echo $height?>px;
 	text-align: center;
 }
 
-.AmiantImageGallerySlideInfo<?php  echo $bID?> {
+.AmiantImageGallerySlideInfo<?php   echo $randID?> {
 	width: 100%;
 	background: black;
 	opacity: 0.7;
@@ -191,23 +192,23 @@
 	padding: 10px;
 }
 
-.AmiantImageGallerySlideControllsWrapper<?php  echo $bID?> {
+.AmiantImageGallerySlideControllsWrapper<?php   echo $randID?> {
 	float: right;
 }
 
-.AmiantImageGallerySlideControllPrev<?php  echo $bID?> {
+.AmiantImageGallerySlideControllPrev<?php   echo $randID?> {
 	font-weight: bold;
 	color: green;
 	cursor: pointer;
 }
 
-.AmiantImageGallerySlideControllNext<?php  echo $bID?> {
+.AmiantImageGallerySlideControllNext<?php   echo $randID?> {
 	font-weight: bold;
 	color: green;
 	cursor: pointer;
 }
 
-#AmiantImageGalleryImagesLoadingWaiter<?php  echo $bID?> {
+#AmiantImageGalleryImagesLoadingWaiter<?php   echo $randID?> {
 	display: block;
 	width: 16px;
 	height: 16px;
@@ -216,10 +217,10 @@
 	float: left;
 }
 
-.AmiantImageGallerySlideControllNav<?php echo $bID?> { margin: 10px; position: relative }
-.AmiantImageGallerySlideControllNav<?php echo $bID?> li { float: left; list-style: none}
+.AmiantImageGallerySlideControllNav<?php  echo $randID?> { margin: 10px; position: relative }
+.AmiantImageGallerySlideControllNav<?php  echo $randID?> li { float: left; list-style: none}
 
-.AmiantImageGallerySlideControllNav<?php echo $bID?> a {
+.AmiantImageGallerySlideControllNav<?php  echo $randID?> a {
 	margin: 2px 1px 3px 0px;
 	padding: 3px 5px;
 	border: 1px solid #CCCCCC;
@@ -233,13 +234,13 @@
 	text-decoration: none;
 }
 
-.AmiantImageGallerySlideControllNav<?php echo $bID?> a:hover {
+.AmiantImageGallerySlideControllNav<?php  echo $randID?> a:hover {
 	margin: 5px 1px 0px 0px;
 	background: #F0F0F0;
 	box-shadow: none;
 }
 
-.AmiantImageGallerySlideControllNav<?php echo $bID?> li a.activeSlide {
+.AmiantImageGallerySlideControllNav<?php  echo $randID?> li a.activeSlide {
 	margin: 5px 1px 0px 0px;
 	background: #555555;
 	border: none;
@@ -247,7 +248,7 @@
 	color: #FFFFFF;
 }
 
-.AmiantImageGallerySlideControllNav<?php echo $bID?> a:focus { outline: none; }
+.AmiantImageGallerySlideControllNav<?php  echo $randID?> a:focus { outline: none; }
 
 .ig_pagination {clear: both; padding: 5px;}
 .ig_pagination_controls {float: right;}
